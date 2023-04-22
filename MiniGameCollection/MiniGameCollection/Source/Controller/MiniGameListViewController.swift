e488117a0bac751e0f6179db5a1b8ae75f52c20a
Gundy <korea1880@naver.com>
2023-04-22 22:27:25 +0900

    feat: MiniGameListViewController의 초기 설정을 하는 configure 메서드 및 miniGamesCollectionView 구현

--- MiniGameCollection/MiniGameCollection/Source/Controller/MiniGameListViewController.swift
+++ MiniGameCollection/MiniGameCollection/Source/Controller/MiniGameListViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class MiniGameListViewController: UIViewController {
 
+    private var miniGames: [Playable] = []
+    private var miniGamesCollectionView: UICollectionView = {
+        let listConfiguration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        let listLayout = UICollectionViewCompositionalLayout.list(using: listConfiguration)
+
+        return UICollectionView(frame: .zero, collectionViewLayout: listLayout)
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        configure()
     }
 
+    private func configure() {
+        configureNavigationBar()
+        configureUI()
+    }
 
+    private func configureNavigationBar() {
+        navigationItem.title = "Mini Games Collection"
+    }
+
+    private func configureUI() {
+        view.backgroundColor = .systemBackground
+        configureLayout()
+    }
+
+    private func configureLayout() {
+        view.addSubview(miniGamesCollectionView)
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            miniGamesCollectionView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            miniGamesCollectionView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            miniGamesCollectionView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            miniGamesCollectionView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
+    }
 }
-
