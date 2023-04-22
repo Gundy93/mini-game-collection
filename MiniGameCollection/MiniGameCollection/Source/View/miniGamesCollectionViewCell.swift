5a847a416a607faa5b71103879ac6d1d2e12af13
Gundy <korea1880@naver.com>
2023-04-22 23:11:53 +0900

    feat: UICollectionViewListCell을 상속하는 miniGamesCollectionViewCell 구현

---
+++ MiniGameCollection/MiniGameCollection/Source/View/miniGamesCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  miniGamesCollectionViewCell.swift
+//  MiniGameCollection
+//
+//  Created by Gundy on 2023/04/22.
+//
+
+import UIKit
+
+class miniGamesCollectionViewCell: UICollectionViewListCell {
+
+    static let identifiet: String = String(describing: miniGamesCollectionViewCell.self)
+}
