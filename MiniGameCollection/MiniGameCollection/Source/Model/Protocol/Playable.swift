7c983a50027bdbfcc706722030550d6da301d3ce
Gundy <korea1880@naver.com>
2023-04-22 21:47:09 +0900

    feat: 게임의 공통 특징을 추상화하는 Playable 프로토콜 작성

---
+++ MiniGameCollection/MiniGameCollection/Source/Model/Protocol/Playable.swift
@@ -0,0 +1,12 @@
+//
+//  Playable.swift
+//  MiniGameCollection
+//
+//  Created by Gundy on 2023/04/17.
+//
+
+protocol Playable {
+
+    var name: String { get }
+    var description: String { get }
+}
