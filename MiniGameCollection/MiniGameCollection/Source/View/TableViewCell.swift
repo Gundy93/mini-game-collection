ba8cc0e376c3b65c5dbc120b18b40f1751642832
Gundy <korea1880@naver.com>
2023-05-01 22:35:09 +0900

    feat: UITableViewCell을 상속하는 TableViewCell 구현

---
+++ MiniGameCollection/MiniGameCollection/Source/View/TableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.swift
+//  MiniGameCollection
+//
+//  Created by Gundy on 2023/04/25.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    static let identifiet: String = String(describing: TableViewCell.self)
+}
