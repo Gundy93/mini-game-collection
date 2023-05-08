933fc29ff980da0531a66ef2e7abed15eaf0711b
Gundy <korea1880@naver.com>
2023-05-08 22:23:31 +0900

    chore: Cell 타입들에 final 수정자 추가

---
+++ MiniGameCollection/MiniGameCollection/Source/View/CollectionViewListCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewListCell.swift
+//  MiniGameCollection
+//
+//  Created by Gundy on 2023/04/22.
+//
+
+import UIKit
+
+final class CollectionViewListCell: UICollectionViewListCell {
+
+    static let identifiet: String = String(describing: CollectionViewListCell.self)
+}
